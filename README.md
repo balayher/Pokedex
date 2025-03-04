4c1afb7307530e349ddb86d61cde2257c4eeafeb
balayher <balayher@gmail.com>
2025-03-04 01:59:41 -0800

    adding exit and help commands

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pokedex
+
+Pokemon Pokedex CLI project made for boot.dev course.
--- repl.go
+++ repl.go
@@ -1,12 +1,61 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, ok := getCommands()[commandName]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
 func cleanInput(text string) []string {
-	trimmedText := strings.TrimSpace(text)
-	lowerText := strings.ToLower(trimmedText)
+	lowerText := strings.ToLower(text)
 	words := strings.Fields(lowerText)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
